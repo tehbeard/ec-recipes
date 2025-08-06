804b2a1718a27aa26c4701341fdfe054da68ec16
tehbeard <tehbeard@gmail.com>
2025-08-06 09:11:40 +0100

    feat: Amethyst Storage Recipe

--- scripts/generate-unstorage-recipes.ts
+++ scripts/generate-unstorage-recipes.ts
@@ -9,7 +9,8 @@ const storage_types = [
     ['nether_wart_block', 'nether_wart', 9],
     ['glowstone', 'glowstone_dust', 4],
     ['bamboo_block', 'bamboo', 9],
-    ['leather','rabbit_hide', 4]
+    ['leather','rabbit_hide', 4],
+    ['amethyst_block','amethyst_shard', 4]
 ]
 
 
