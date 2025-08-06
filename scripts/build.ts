da58cc8f7a8ca96fb9be5369a7073e98c8c4da68
tehbeard <tehbeard@gmail.com>
2025-08-06 09:11:44 +0100

    feat: Add the wild reset compass recipe

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,3 +3,11 @@ import './generate-paintings.ts';
 import './generate-stonecutter-wood.ts';
 import './generate-unstorage-recipes.ts';
 import './slab.ts';
+import { writeJSON } from './utils.ts';
+
+// Reset compass recipe to vanilla
+// Needed as the wild compass has to turn it into a lodestone compass, this reverts that.
+writeJSON(
+    './data/escapecraft/recipe/compass_reset.json',
+    {"type":"minecraft:crafting_shapeless","ingredients":["minecraft:compass"],"result":{"id":"minecraft:compass"}}
+);
